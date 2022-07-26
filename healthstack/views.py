718d72fb5f2ff0a4c43e426c3d470b79c6837bf2
Mohammed Jawwadul Islam Fida <fidajisa@hotmail.com>
2022-07-26 18:41:02 +0600

    add gitignore

--- healthstack/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render , redirect
-from django.contrib.auth.models import authenticate , login, logout
-from django.contrib.auth.decorators import login_required
-
--- hospital/views.py
+++ hospital/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+
 def hospital_home(request):
     return render(request, 'index.html')
 
+# Create your views here.
 
+# function to return views for the urls
