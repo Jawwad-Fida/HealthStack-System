bc754f6c2a5051b26ab1d2e62c2add290ef4b032
Rafnarok335 <mrafi181201@bscse.uiu.ac.bd>
2022-07-27 01:59:06 +0600

    admin renamed to hospital_admin

--- hospital/urls.py
+++ hospital/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
 # from . --> same directory
 # Views functions and urls must be linked. # of views == # of urls
@@ -9,7 +10,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.hospital_home),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
