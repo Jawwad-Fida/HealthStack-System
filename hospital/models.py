701aa4a4ff0997696b0e941cdd35cff3a819842e
Mohammed Jawwadul Islam Fida <fidajisa@hotmail.com>
2022-07-26 18:52:15 +0600

    test 1

--- hospital/models.py
+++ hospital/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
-#import uuid
-
 # Create your models here.
-
-# django automatically creates an id field for each model (int)
-# null=True --> don't require a value when inserting into the database
-# blank=True --> allow blank value when submitting a form
-# auto_now_add --> automatically set the value to the current date and time
-# unique=True --> prevent duplicate values
-# primary_key=True --> set this field as the primary key
-# editable=False --> prevent the user from editing this field
--- hospital/views.py
+++ hospital/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-
 def hospital_home(request):
     return render(request, 'index.html')
 
-# Create your views here.
 
-# function to return views for the urls
