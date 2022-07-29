832141c3fd428f8998ad9a97f2dde9c00fc84075
Rafnarok335 <mrafi181201@bscse.uiu.ac.bd>
2022-07-29 10:45:16 +0600

    Hospital Url conflict

--- hospital/urls.py
+++ hospital/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # urlpatterns = [
 
