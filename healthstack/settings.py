28648f3149a48ae79ce400cc65ee62581c935917
pranto <ppodder181202@bscse.uiu.ac.bd>
2022-07-30 23:00:21 +0600

    index page modified

--- healthstack/settings.py
+++ healthstack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--%)2&l2zj#1jj-co*+9xh-#+z6y5xjdym)7m9%^y935l8=qx6t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.101']
 
 
 # Application definition
