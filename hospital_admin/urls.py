5c02d077c036f691fb6939c8c656c50574b9d32c
mimo85 <mimokabir85@gmail.com>
2022-08-10 22:21:30 +0600

    admin transaction and appointment page linked

--- hospital_admin/urls.py
+++ hospital_admin/urls.py
@@ -28,12 +28,13 @@ urlpatterns = [
     path('patient-list/', views.patient_list, name='patient_list'),
     path('profile/', views.admin_profile, name='admin_profile'),
     path('register/', views.admin_register, name='admin_register'),
-    path('transactions-list/', views.transactions_list, name='transactions_list'),
     path('admin-logout/', views.logoutAdmin, name='admin-logout'),
 
     path('emergency/', views.emergency_details, name='emergency'),
     path('add-emergency-information/', views.add_emergency_information,
          name='add-emergency-information'),
+    path('appointment-list/', views.appointment_list, name='appointment-list'),
+    path('transactions-list/', views.transactions_list, name='transactions-list'),
 
 
 
