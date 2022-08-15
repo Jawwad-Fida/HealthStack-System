a2bdb26bd9b71d7a6f8a8f4b55785e7f38bb153a
Mohammed Jawwadul Islam Fida <fidajisa@hotmail.com>
2022-08-15 17:22:39 +0600

    Revert to fdeca24017e25c12742e0c81bc545520b444284f

---
+++ static/HealthStack-System/js/Normal/sidebar.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelectorAll('.sidebar .nav-link').forEach(function(element){
+      
+      element.addEventListener('click', function (e) {
+  
+        let nextEl = element.nextElementSibling;
+        let parentEl  = element.parentElement;	
+  
+          if(nextEl) {
+              e.preventDefault();	
+              let mycollapse = new bootstrap.Collapse(nextEl);
+              
+              if(nextEl.classList.contains('show')){
+                mycollapse.hide();
+              } else {
+                  mycollapse.show();
+                  // find other submenus with class=show
+                  var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                  // if it exists, then close all of them
+                  if(opened_submenu){
+                    new bootstrap.Collapse(opened_submenu);
+                  }
+              }
+          }
+      }); // addEventListener
+    }) // forEach
+  }); 
