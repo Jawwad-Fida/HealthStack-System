0a1e256c9be9561aedbcac24138989478b2e9bea
Mohammed Jawwadul Islam Fida <fidajisa@hotmail.com>
2022-08-17 22:58:14 +0600

    labworker table added and patient table modified

--- hospital/models.py
+++ hospital/models.py
@@ -76,9 +76,9 @@ class Patient(models.Model):
         max_length=200, null=True, blank=True)
     # patient_allergy = models.TextField(null=True, blank=True)
     history = models.CharField(max_length=200, null=True, blank=True)
-
     dob = models.DateField(null=True, blank=True)
     nid = models.CharField(max_length=200, null=True, blank=True)
+    serial_number = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return str(self.user.username)
