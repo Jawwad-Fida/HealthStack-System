8a9c0f5ca3f6ff2fe807a233e23d15f727d19316
aysha-siddika-nowshin <ayshasiddikan@gmail.com>
2022-09-05 18:57:25 +0600

    report test result

--- static/HealthStack-System/js/Normal/report-settings.js
+++ static/HealthStack-System/js/Normal/report-settings.js
@@ -51,4 +51,49 @@ Version      : 1.0
         $(".specimen-info").append(specimencontent);
         return false;
     });		
+
+	// Test Add More
+	
+    $(".test-info").on('click','.trash', function () {
+		$(this).closest('.test-cont').remove();
+		return false;
+    });
+
+    $(".add-test").on('click', function () {  
+		
+		var testcontent = '<div class="row form-row test-cont">' +
+			'<div class="col-12 col-md-10 col-lg-11">' +
+				'<div class="row form-row">' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>Test Name</label>' +
+							'<input type="text" name="test_name" class="form-control">' +
+						'</div>' +
+					'</div>' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>Result</label>' +
+							'<input type="text" name="result" class="form-control">' +
+						'</div>' +
+					'</div>' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>Unit</label>' +
+							'<input type="text" name="unit" class="form-control">' +
+						'</div>' +
+					'</div>' +
+                    '<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>Referred Value</label>' +
+							'<input type="text" name="referred_value" class="form-control">' +
+						'</div>' +
+					'</div>' +
+				'</div>' +
+			'</div>' +
+			'<div class="col-12 col-md-2 col-lg-1"><label class="d-md-block d-sm-none d-none">&nbsp;</label><a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a></div>' +
+		'</div>';
+		
+        $(".test-info").append(testcontent);
+        return false;
+    });		
 })(jQuery);
