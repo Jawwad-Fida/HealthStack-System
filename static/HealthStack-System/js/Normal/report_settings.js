6d2088a3509bcae29906ce7f7afbdeb155ec5eb1
pranto <ppodder181202@bscse.uiu.ac.bd>
2022-09-06 22:50:01 +0600

    create invoice js & css file fixed!

--- static/HealthStack-System/js/Normal/report_settings.js
+++ static/HealthStack-System/js/Normal/report_settings.js
@@ -19,27 +19,27 @@ Version      : 1.0
 		var specimencontent = '<div class="row form-row specimen-cont">' +
 			'<div class="col-12 col-md-10 col-lg-11">' +
 				'<div class="row form-row">' +
-					'<div class="col-12 col-md-6 col-lg-4">' +
+					'<div class="col-12 col-md-6 col-lg-3">' +
 						'<div class="form-group">' +
 							'<label>Specimen ID</label>' +
 							'<input type="text" name="specimen_ID" class="form-control">' +
 						'</div>' +
 					'</div>' +
-					'<div class="col-12 col-md-6 col-lg-4">' +
+					'<div class="col-12 col-md-6 col-lg-3">' +
 						'<div class="form-group">' +
 							'<label>Specimen Type</label>' +
 							'<input type="text" name="specimen_type" class="form-control">' +
 						'</div>' +
 					'</div>' +
-					'<div class="col-12 col-md-6 col-lg-4">' +
+					'<div class="col-12 col-md-6 col-lg-3">' +
 						'<div class="form-group">' +
-							'<label>Collection Date/Time</label>' +
+							'<label>Collection Date</label>' +
 							'<input type="text" name="collection_date" class="form-control">' +
 						'</div>' +
 					'</div>' +
-                    '<div class="col-12 col-md-6 col-lg-4">' +
+                    '<div class="col-12 col-md-6 col-lg-3">' +
 						'<div class="form-group">' +
-							'<label>Receiving Date/Time</label>' +
+							'<label>Receiving Date</label>' +
 							'<input type="text" name="receiving_date" class="form-control">' +
 						'</div>' +
 					'</div>' +
@@ -64,25 +64,25 @@ Version      : 1.0
 		var testcontent = '<div class="row form-row test-cont">' +
 			'<div class="col-12 col-md-10 col-lg-11">' +
 				'<div class="row form-row">' +
-					'<div class="col-12 col-md-6 col-lg-4">' +
+					'<div class="col-12 col-md-6 col-lg-3">' +
 						'<div class="form-group">' +
 							'<label>Test Name</label>' +
 							'<input type="text" name="test_name" class="form-control">' +
 						'</div>' +
 					'</div>' +
-					'<div class="col-12 col-md-6 col-lg-4">' +
+					'<div class="col-12 col-md-6 col-lg-3">' +
 						'<div class="form-group">' +
 							'<label>Result</label>' +
 							'<input type="text" name="result" class="form-control">' +
 						'</div>' +
 					'</div>' +
-					'<div class="col-12 col-md-6 col-lg-4">' +
+					'<div class="col-12 col-md-6 col-lg-3">' +
 						'<div class="form-group">' +
 							'<label>Unit</label>' +
 							'<input type="text" name="unit" class="form-control">' +
 						'</div>' +
 					'</div>' +
-                    '<div class="col-12 col-md-6 col-lg-4">' +
+                    '<div class="col-12 col-md-6 col-lg-3">' +
 						'<div class="form-group">' +
 							'<label>Referred Value</label>' +
 							'<input type="text" name="referred_value" class="form-control">' +
