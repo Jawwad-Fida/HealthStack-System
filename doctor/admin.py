ae0f79076764cc32667f61711cf28dc03462b55d
Mohammed Jawwadul Islam Fida <fidajisa@hotmail.com>
2022-09-12 14:29:40 +0600

    register new models in admin side

--- doctor/admin.py
+++ doctor/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 # # we are in same file path --> .models
 
-from .models import Doctor_Information, Appointment, Report, Prescription, Education, Experience, Specimen, Test,perscription_medicine,perscription_test
+from .models import Doctor_Information, Appointment, Report, Prescription, Education, Experience, Specimen, Test,perscription_medicine,perscription_test,test_Cart,test_Order
 
 admin.site.register(Doctor_Information)
 admin.site.register(Appointment)
@@ -15,3 +15,5 @@ admin.site.register(Test)
 admin.site.register(Specimen)
 admin.site.register(perscription_medicine)
 admin.site.register(perscription_test)
+admin.site.register(test_Cart)
+admin.site.register(test_Order)
