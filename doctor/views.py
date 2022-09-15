3ba5b540cb71b682aa0e9636726863e7625537cc
pranto <ppodder181202@bscse.uiu.ac.bd>
2022-09-15 17:48:55 +0600

    test cart price added

--- doctor/views.py
+++ doctor/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views.decorators.cache import cache_control
 from hospital.models import User, Patient
-from hospital_admin.models import Admin_Information,Clinical_Laboratory_Technician
+from hospital_admin.models import Admin_Information,Clinical_Laboratory_Technician, Test_Information
 from .models import Doctor_Information, Appointment, Education, Experience, Prescription_medicine, Report,Specimen,Test, Prescription_test, Prescription
 
 from django.db.models import Q, Count
@@ -467,6 +467,7 @@ def create_prescription(request,pk):
             patient = Patient.objects.get(patient_id=pk) 
             create_date = datetime.date.today()
             
+
             if request.method == 'POST':
                 prescription = Prescription(doctor=doctor, patient=patient)
                 
@@ -480,6 +481,8 @@ def create_prescription(request,pk):
                 medicine_instruction = request.POST.getlist('instruction')
                 extra_information = request.POST.get('extra_information')
                 test_info_id = request.POST.getlist('id')
+
+                
 
             
                 prescription.extra_information = extra_information
@@ -502,6 +505,9 @@ def create_prescription(request,pk):
                     tests.test_name = test_name[i]
                     tests.test_description = test_description[i]
                     tests.test_info_id = test_info_id[i]
+                    test_info = Test_Information.objects.get(test_id=test_info_id[i])
+                    tests.test_info_price = test_info.test_price
+                   
                     tests.save()
 
                 return redirect('patient-profile', pk=patient.patient_id)
